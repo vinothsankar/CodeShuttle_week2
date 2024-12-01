ce4592a59bcbc0e3bcbac005dd35351381ada4c7
vinothkumar <47396343+vinothsankar@users.noreply.github.com>
2024-12-01 21:30:12 +0530

    create Entity and Dto

--- src/main/java/com/example/Department/annotations/PrimeNumberValidation.java
+++ src/main/java/com/example/Department/annotations/PrimeNumberValidation.java
@@ -2,7 +2,6 @@ package com.example.Department.annotations;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -13,7 +12,9 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = {PrimeNumberValidator.class})
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 public @interface PrimeNumberValidation {
+
     String message() default "Number should be Prime Number";
     Class<?>[] groups() default { };
     Class<? extends Payload>[] Payload() default { };
+
  }
---
+++ src/main/java/com/example/Department/dto/DepartmentDto.java
@@ -0,0 +1,44 @@
+package com.example.Department.dto;
+
+import com.example.Department.annotations.PasswordValidation;
+import com.example.Department.annotations.PrimeNumberValidation;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.CreditCardNumber;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class DepartmentDto {
+
+    private long id;
+
+    @Size(min=3, message = "No of character should be > 3")
+    private String name;
+
+    @Min(value = 18, message = "Age should be 18-45")
+    @Max(value = 45, message = "Age should be 18-45")
+    private int age;
+
+    @Email(message = "Enter the valid Email")
+    private String email;
+
+    @PrimeNumberValidation()
+    private int primNumber;
+
+    @CreditCardNumber(message = "Enter a valid Credit card number")
+    private long creteCard;
+
+    @AssertTrue
+    private boolean isActive;
+
+    @JsonFormat(pattern = "DD-MM-YYYY hh:mm:ss")
+    private LocalDate createdAt;
+
+    @PasswordValidation()
+    private String password;
+}
